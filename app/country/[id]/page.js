970d1b5088ef9bc75e636dee30dcca1cdbea5c23
BaironDev7 <baironbotero15@gmail.com>
2024-11-17 18:35:14 -0500

    Separation of API Data for divs

--- app/country/[id]/page.js
+++ app/country/[id]/page.js
@@ -14,49 +14,60 @@ export default async function CountryDetails({ params }) {
 
   return (
     <div className="container mx-auto p-4 text-black">
-      <h1 className="text-3xl font-bold mb-4 text-green-600">CountrInfo</h1>
+      <h1 className="text-4xl text-center font-bold mb-4 text-green-600">CountrInfo</h1>
       <h1 className="text-3xl font-bold mb-4">{country.name.common}</h1>
-      <h3>Bandera:</h3>
-      <img src={country.flags.png} alt={`Bandera de ${country.name.common}`} className="w-64 h-auto mb-4" />
-      <p><strong>Nombre Oficial:</strong> {country.name.official}</p>
-      <p><strong>Capital:</strong> {country.capital}</p>
-      <p><strong>Continente:</strong> {country.region}</p>
-      <p><strong>Subregión:</strong> {country.subregion}</p>
-      <p><strong>Raíz:</strong> {country.idd.root}</p>
-      <p><strong>Sufijo:</strong> {country.idd.suffixes}</p>
-      <p><strong>Dominio:</strong> {country.tld}</p>
-      <p><strong>Abreviación:</strong> {country.cca3}</p>
-      <p><strong>Población:</strong> {country.population.toLocaleString()}</p>
-      <p><strong>Área:</strong> {country.area.toLocaleString()} km²</p>
-      <p><strong>País Independiente:</strong> {country.independent ? 'País independiente' : 'País No independiente'}</p>
-      <p><strong>Zona Horaria:</strong> {country.timezones}</p>
-      <p><strong>Latitud y Longitud:</strong> {country.latlng}</p>
-      <p><strong>Area (km²):</strong> {country.area}</p>
-      <p><strong>Cercado de tierra:</strong> {country.landlocked ? 'Cercado por tierra' : 'No cercado por tierra'}</p>
-      <img src={country.coatOfArms.png} alt={`Escudo de ${country.name.common}`} className="w-64 h-auto mb-4" />
-      <p><strong>Moneda(s):</strong> {
-      country.currencies 
-        ? Object.keys(country.currencies).map((currencyKey) => {
-        const currency = country.currencies[currencyKey];
-        return (
-          <span key={currencyKey}>
-            {currencyKey} - {currency.name} ({currency.symbol})
-          </span>
-          );
-        })
-        : 'No data'
-      }</p>
       <div>
-        <strong>Lenguajes:</strong>
-        {country.languages ? (
-          <ul>
-            {Object.values(country.languages).map((language, index) => (
-              <li key={index}>{language}</li>
-            ))}
-          </ul>
-        ) : (
-          <p>No hay información de lenguajes.</p>
-        )}
+        <strong>Bandera:</strong>
+        <img src={country.flags.png} alt={`Bandera de ${country.name.common}`} className="w-64 h-auto mb-4" />
+      </div>
+      <div>
+        <strong>Datos Generales:</strong>
+        <p><strong>Nombre Oficial:</strong> {country.name.official}</p>
+        <p><strong>Capital:</strong> {country.capital}</p>
+        <p><strong>Población:</strong> {country.population.toLocaleString()}</p>
+        <p><strong>Moneda(s):</strong> {
+          country.currencies 
+          ? Object.keys(country.currencies).map((currencyKey) => {
+          const currency = country.currencies[currencyKey];
+          return (
+            <span key={currencyKey}>
+              {currencyKey} - {currency.name} ({currency.symbol})
+            </span>
+            );
+          })
+          : 'No data'
+        }</p>
+        <p><strong>Continente:</strong> {country.region}</p>
+        <p><strong>Subregión:</strong> {country.subregion}</p>
+        <p><strong>País Independiente:</strong> {country.independent ? 'País independiente' : 'País No independiente'}</p>
+      </div>
+      <div>
+        <strong>Datos de terreno:</strong>
+        <p><strong>Área:</strong> {country.area.toLocaleString()} km²</p>
+        <p><strong>Latitud y Longitud:</strong> {country.latlng}</p>
+        <p><strong>Cercado de tierra:</strong> {country.landlocked ? 'Cercado por tierra' : 'No cercado por tierra'}</p>
+      </div>
+      <div>
+        <strong>Otros Datos:</strong>
+        <p><strong>Raíz:</strong> {country.idd.root}</p>
+        <p><strong>Sufijo:</strong> {country.idd.suffixes}</p>
+        <p><strong>Dominio:</strong> {country.tld}</p>
+        <p><strong>Abreviación:</strong> {country.cca3}</p>
+        <p><strong>Zona Horaria:</strong> {country.timezones}</p>
+        <strong>Escudo:</strong>
+        <img src={country.coatOfArms.png} alt={`Escudo de ${country.name.common}`} className="w-64 h-auto mb-4" />
+        <div>
+          <strong>Lenguajes:</strong>
+          {country.languages ? (
+            <ul>
+              {Object.values(country.languages).map((language, index) => (
+                <li key={index}>{language}</li>
+              ))}
+            </ul>
+          ) : (
+            <p>No hay información de lenguajes.</p>
+          )}
+        </div>
       </div>
 
       <div className="mt-4">
